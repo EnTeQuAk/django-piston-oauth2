0e0dbb91e6387fe720a375bef4d290ff91573bb6
Allen Short <ashort@mozilla.com>
2014-01-24 23:51:44 -0600

    Incorporate fix for django-piston #164

--- piston/emitters.py
+++ piston/emitters.py
@@ -100,6 +100,16 @@ class Emitter(object):
             Dispatch, all types are routed through here.
             """
             ret = None
+            # return anything we've already seen as a string only
+            # this prevents infinite recursion in the case of recursive
+            # relationships
+
+            if thing in self.stack:
+                raise RuntimeError, (u'Circular reference detected while emitting '
+                                     'response')
+
+            self.stack.append(thing)
+
 
             if isinstance(thing, QuerySet):
                 ret = _qs(thing, fields)
@@ -124,7 +134,7 @@ class Emitter(object):
                 ret = _any(thing.all())
             else:
                 ret = smart_unicode(thing, strings_only=True)
-
+            self.stack.pop()
             return ret
 
         def _fk(data, field):
@@ -297,6 +307,7 @@ class Emitter(object):
             return dict([ (k, _any(v, fields)) for k, v in data.iteritems() ])
 
         # Kickstart the seralizin'.
+        self.stack = []
         return _any(self.data, self.fields)
 
     def in_typemapper(self, model, anonymous):
